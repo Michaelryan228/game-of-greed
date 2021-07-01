30c1de62a893612535e41cacf5dca6861e97af11
Daniel Dills <dills253@gmail.com>
2021-06-30 20:57:43 -0700

    completed lab 07

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -9,13 +9,14 @@ class Game:
     self.dice_left = 6
 
 
-  def play(self, roller=GameLogic.roll_dice):
+  def play(self, roller):
     # Use roller(dice) wherever we need to roll dice.
-    # self.roller = roller or GameLogic.roll_dice
+    self.roller = roller or GameLogic.roll_dice
     # help w/ this from Anthony Beaver
     # self.roller can be whatever we input as roller
     # OR it can call the static function from GameLogic
-    banker = Banker() 
+    banker = Banker()
+    # Initialize the game with a banker
 
     print('Welcome to Game of Greed')
     print('(y)es to play or (n)o to decline')
@@ -33,18 +34,16 @@ class Game:
       # ROUND 1 #
 
       print(f'Starting round {self.round}') # keeps track of round
-      print(f'Rolling {self.dice_left} dice...') # says how many dice are left
       # print(f'*** {self.roller(self.dice_left)} ***') <-- prints LIST of roll, save for future reference
-
-      # next line is ONLY for running test_one_and_done
-      # print('*** 4 4 5 2 3 1 ***')
-
-      # next line is ONLY for running test_single_bank
-      print('*** 4 2 6 4 6 5 ***') 
+      print(f'Rolling {self.dice_left} dice...')
+      roll = self.roller(self.dice_left)
+      printed_results = GameLogic.to_str(roll)
+      print(f'*** {printed_results}***')
 
       print('Enter dice to keep, or (q)uit:')
       dice_to_keep = input('> ')
       # ON test_bank_first_for_two_rounds USER INPUT IS A LIST?!?!?!
+      users_dice = [x for x in dice_to_keep if x.isdigit()]
 
       if dice_to_keep == 'q':
         print(f'Thanks for playing. You earned {banker.balance} points')
@@ -53,8 +52,8 @@ class Game:
 
       if dice_to_keep != 'q':
         # https://www.kite.com/python/answers/how-to-convert-a-list-of-strings-to-ints-in-python
-        num_of_dice_shelving = len(dice_to_keep)
-        dice_kept = map(int, dice_to_keep) # maps over user input and turns strings to integers
+        num_of_dice_shelving = len(users_dice)
+        dice_kept = map(int, users_dice) # maps over user input and turns strings to integers
         roll_tuple = tuple(dice_kept) # turns user's selected dice into a tuple for scoring
         score = GameLogic.calculate_score(roll_tuple) # scores the user's selected dice
         banker.shelf(score)
@@ -70,24 +69,6 @@ class Game:
         print(f'Total score is {banker.balance} points')
         self.round += 1
         self.dice_left = 6
-
-      # ROUND 2 #
-      # needed to hard code Round 2 instead of dynamically 
-      # looping to hard-code test dice roll for test to pass 
-
-      print(f'Starting round {self.round}')
-      print(f'Rolling {self.dice_left} dice...')
-      print(f'*** 6 4 5 2 3 1 ***')
-
-      print('Enter dice to keep, or (q)uit:')
-      dice_to_keep = input('> ')
-
-      if dice_to_keep == 'q':
-        print(f'Thanks for playing. You earned {banker.balance} points')
-        running = False
-        return
-
-
 
 # ------------------------------- TA TIPS-------------------------------
 
--- tests/test_sim_basic.py
+++ tests/test_sim_basic.py
@@ -10,7 +10,7 @@ def test_quitter():
     diffs = diff(game.play, path="tests/version_2/quitter.sim.txt")
     assert not diffs, diffs
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_one_and_done():
     game = Game()
     diffs = diff(game.play, path="tests/version_2/one_and_done.sim.txt")
@@ -24,7 +24,7 @@ def test_single_bank():
     )
     assert not diffs, diffs
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_bank_first_for_two_rounds():
     game = Game()
     diffs = diff(
